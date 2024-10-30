c755d1d3d1a543c0edbb76a0df2d20cc4d0ba04f
Conor Griffin <115548358+YoungGriff11@users.noreply.github.com>
2024-10-30 14:17:20 +0000

    Sixth Commit

--- cv.js
+++ cv.js
@@ -31,9 +31,9 @@ function openModal(project) {
             description: 'A mock food delivery service website for multiple restaraunts incorporating a basket system.',
             techStack: 'HTML, CSS, JavaScript, VS Code'
         },
-        'Urban & Co Barbers': {
+        'Urban-&-Co-Barbers': {
             title: 'Urban & Co Barbers',
-            description: 'A website I was compensated for building and running, built to just display the services and items available in store.',
+            description: 'A website I was compensated for building and running, built to display the services and items available in store.',
             techStack: 'HTML, CSS, JavaScript, VS Code'
         }
     };
